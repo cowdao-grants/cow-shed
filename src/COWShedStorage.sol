34afe51151acfd8f46e92634b40253dd52354434
Meet Mangukiya <meet@flamy.dev>
2024-05-15 18:56:01 +0530

    perf: use Bitmap for gas efficiency in sequential nonces

--- src/COWShedStorage.sol
+++ src/COWShedStorage.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.25;
 
+import { LibBitmap } from "solady/utils/LibBitmap.sol";
+
 /// @dev bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1)
 bytes32 constant IMPLEMENTATION_STORAGE_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
@@ -9,10 +11,12 @@ interface IAdminView {
 }
 
 contract COWShedStorage {
+    using LibBitmap for LibBitmap.Bitmap;
+
     struct State {
         bool initialized;
         address trustedExecutor;
-        mapping(bytes32 => bool) nonces;
+        LibBitmap.Bitmap nonces;
     }
 
     bytes32 internal constant STATE_STORAGE_SLOT = keccak256("COWShed.State");
@@ -26,5 +30,13 @@ contract COWShedStorage {
 
     function _admin() internal view returns (address) {
         return IAdminView(address(this)).admin();
+    }
+
+    function _useNonce(bytes32 _nonce) internal {
+        _state().nonces.set(uint256(_nonce));
+    }
+
+    function _isNonceUsed(bytes32 _nonce) internal view returns (bool) {
+        return _state().nonces.get(uint256(_nonce));
     }
 }
