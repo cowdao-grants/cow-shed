6f81590cc79bc38560a93df0b55a8260f6b0eded
Meet Mangukiya <meet@flamy.dev>
2024-05-28 17:48:37 +0530

    change assembly to solidity

--- src/LibAuthenticatedHooks.sol
+++ src/LibAuthenticatedHooks.sol
@@ -56,19 +56,7 @@ library LibAuthenticatedHooks {
         returns (bytes32 _toSign)
     {
         bytes32 messageHash = executeHooksMessageHash(calls, nonce, deadline);
-
-        assembly {
-            let freeMemoryPointer := mload(0x40)
-
-            mstore(0x00, 0x1901)
-            mstore(0x20, domainSeparator)
-            mstore(0x40, messageHash)
-
-            _toSign := keccak256(0x1e, 0x42)
-
-            // restore free memory pointer
-            mstore(0x40, freeMemoryPointer)
-        }
+        _toSign = keccak256(abi.encodePacked(bytes2(0x1901), domainSeparator, messageHash));
     }
 
     /// @dev the `hashStruct` encoded hash for the given `ExecuteHooks` message.
@@ -79,20 +67,7 @@ library LibAuthenticatedHooks {
     {
         bytes32 callshash = callsHash(calls);
         bytes32 executeHooksTypeHash = EXECUTE_HOOKS_TYPE_HASH;
-
-        assembly {
-            let before := mload(0x40)
-            mstore(0x00, executeHooksTypeHash)
-            mstore(0x20, callshash)
-            mstore(0x40, nonce)
-            mstore(0x60, deadline)
-            hash := keccak256(0x00, 0x80)
-
-            // restore free memory pointer
-            mstore(0x40, before)
-            // restore the zero slot
-            mstore(0x60, 0)
-        }
+        hash = keccak256(abi.encode(executeHooksTypeHash, callshash, nonce, deadline));
     }
 
     /// @dev the `encodeData` output for the provided calls' dynamic array.
@@ -121,28 +96,7 @@ library LibAuthenticatedHooks {
         bool isDelegateCall = cll.isDelegateCall;
         bytes32 callTypeHash = CALL_TYPE_HASH;
 
-        assembly {
-            let freeMemoryPointer := mload(0x40)
-            let firstSlot := mload(0x80)
-            let secondSlot := mload(0xa0)
-
-            mstore(0x00, callTypeHash)
-            mstore(0x20, target)
-            mstore(0x40, value)
-            mstore(0x60, callDataHash)
-            mstore(0x80, allowFailure)
-            mstore(0xa0, isDelegateCall)
-            _callHash := keccak256(0x00, 0xc0)
-
-            // restore free memory pointer
-            mstore(0x40, freeMemoryPointer)
-            // restore 0 slot
-            mstore(0x60, 0x00)
-            // restore first slot
-            mstore(0x80, firstSlot)
-            // restore second slot
-            mstore(0xa0, secondSlot)
-        }
+        _callHash = keccak256(abi.encode(callTypeHash, target, value, callDataHash, allowFailure, isDelegateCall));
     }
 
     /// @dev execute given calls
