03353a3af055d0118c4bb8cf0e2c68a27aed6e35
Anxo Rodriguez <anxolin@gmail.com>
2025-08-06 19:57:18 +0100

    Allow network json to be manually generated

---
+++ dev/generate-networks-file.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o errexit -o pipefail -o nounset
+
+repo_root_dir="$(git rev-parse --show-toplevel)"
+manual_file="$repo_root_dir/networks-manual.json"
+
+# Generate JSON from broadcast deployment files
+generated=$(for deployment in "$repo_root_dir/broadcast/"*"/"*"/"*".json"; do
+  # Extract chain ID from folder name
+  chain_id=${deployment%/*}
+  chain_id=${chain_id##*/}
+
+  # Extract contract info per chain
+  jq --arg chainId "$chain_id" '
+    .transactions[]
+    | select(.transactionType == "CREATE2")
+    | select(.hash != null)
+    | {(.contractName): {($chainId): {address: .contractAddress, transactionHash: .hash }}}
+  ' <"$deployment"
+done | jq --sort-keys --null-input 'reduce inputs as $item ({}; . *= $item)')
+
+# Merge with manual file if it exists
+if [[ -f "$manual_file" ]]; then
+  jq -s 'reduce .[] as $item ({}; . *= $item)' \
+    <(echo "$generated") "$manual_file"
+else
+  echo "$generated"
+fi
