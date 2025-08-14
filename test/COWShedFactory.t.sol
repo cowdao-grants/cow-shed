9a98b3ddbab54537c096b0c08c91a1cc6533d48a
Anxo Rodriguez <anxolin@gmail.com>
2025-08-14 20:44:17 +0100

    Add test for factory deployments

--- test/COWShedFactory.t.sol
+++ test/COWShedFactory.t.sol
@@ -5,6 +5,7 @@ import {Test} from "forge-std/Test.sol";
 
 import {DeployScript} from "script/Deploy.s.sol";
 import {COWShed, COWShedFactory, COWShedProxy} from "src/COWShedFactory.sol";
+import {PreSignStateStorage} from "src/PreSignStateStorage.sol";
 
 contract COWShedFactoryTest is Test {
     string constant baseEns = "testcowhooks.eth";
@@ -28,5 +29,88 @@ contract COWShedFactoryTest is Test {
         assertEq(address(expected).code.length, 0);
         factory.initializeProxy(user, false);
         assertGt(address(expected).code.length, 0);
+    }
+
+    function testDeployPreSignStateStorage_success() external {
+        // GIVEN: A valid COWShed address
+        address cowShed = makeAddr("cowShed");
+
+        // WHEN: Deploying a PreSignStateStorage contract for the COWShed
+        address storageContract = factory.deployPreSignStateStorage(cowShed);
+
+        // THEN: The contract should be deployed and have the correct cowShed address
+        assertGt(storageContract.code.length, 0);
+        PreSignStateStorage storageInstance = PreSignStateStorage(storageContract);
+        assertEq(storageInstance.cowShed(), cowShed);
+    }
+
+    function testDeployPreSignStateStorage_redeploy() external {
+        // GIVEN: A valid COWShed address
+        address cowShed = makeAddr("cowShed");
+
+        // GIVEN: A PreSignStateStorage contract already deployed
+        address storageContract1 = factory.deployPreSignStateStorage(cowShed);
+
+        // WHEN: Deploying a PreSignStateStorage contract for the second time
+        address storageContract2 = factory.deployPreSignStateStorage(cowShed);
+
+        // THEN: The contract should be deployed and have the correct cowShed address
+        assertGt(storageContract2.code.length, 0);
+        PreSignStateStorage storageInstance = PreSignStateStorage(storageContract2);
+        assertEq(storageInstance.cowShed(), cowShed);
+
+        // THEN: The redeployment address should be different from the first deployment
+        assertNotEq(storageContract1, storageContract2);
+    }
+
+    function testDeployPreSignStateStorageForUser_success() external {
+        // GIVEN: A test user address
+        address user = makeAddr("user");
+
+        // WHEN: Deploying a PreSignStateStorage contract for the user
+        address storageContract = factory.deployPreSignStateStorageForUser(user);
+
+        // THEN: The contract should be deployed and point to the user's proxy
+        assertGt(storageContract.code.length, 0);
+        address userProxy = factory.proxyOf(user);
+        PreSignStateStorage storageInstance = PreSignStateStorage(storageContract);
+        assertEq(storageInstance.cowShed(), userProxy);
+    }
+
+    function testDeployPreSignStateStorageForUser_redeploy() external {
+        // GIVEN: A test user address
+        address user = makeAddr("user");
+
+        // GIVEN: A PreSignStateStorage contract already deployed
+        address storageContract1 = factory.deployPreSignStateStorageForUser(user);
+
+        // WHEN: Deploying a PreSignStateStorage contract for the second time
+        address storageContract2 = factory.deployPreSignStateStorageForUser(user);
+
+        // THEN: The contract should be deployed and point to the user's proxy
+        assertGt(storageContract2.code.length, 0);
+        address userProxy = factory.proxyOf(user);
+        PreSignStateStorage storageInstance = PreSignStateStorage(storageContract2);
+        assertEq(storageInstance.cowShed(), userProxy);
+
+        // THEN: The redeployment address should be different from the first deployment
+        assertNotEq(storageContract1, storageContract2);
+    }
+
+    function testMultiplePreSignStateStorageDeployments() external {
+        // GIVEN: Two different users
+        address user1 = makeAddr("user1");
+        address user2 = makeAddr("user2");
+
+        // WHEN: Deploying storage contracts for different users
+        address storage1 = factory.deployPreSignStateStorageForUser(user1);
+        address storage2 = factory.deployPreSignStateStorageForUser(user2);
+
+        // THEN: Different contracts should be deployed pointing to correct proxies
+        assertTrue(storage1 != storage2);
+        PreSignStateStorage storageInstance1 = PreSignStateStorage(storage1);
+        PreSignStateStorage storageInstance2 = PreSignStateStorage(storage2);
+        assertEq(storageInstance1.cowShed(), factory.proxyOf(user1));
+        assertEq(storageInstance2.cowShed(), factory.proxyOf(user2));
     }
 }
